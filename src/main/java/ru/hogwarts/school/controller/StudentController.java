65cf3819f58f12c670b7ea0a49f86c69bdc5a9d9
alina <alina.meller.98@mail.ru>
2023-10-31 20:22:00 +0300

    StudentController.java готов и сервис тоже

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,49 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+@RequestMapping("/student")
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Student> createUser(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+        return ResponseEntity.ok(createdStudent);
+    }
+
+    @GetMapping("/userId")
+    public ResponseEntity<Student> getStudent(@PathVariable Long studentId){
+        Student student = studentService.getStudentById(studentId);
+        if (student == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> updateStudent(@RequestBody  Student student){
+        Student updatedStudent = studentService.updateStudent(student.getId(), student);
+    if (updatedStudent == null)
+        return ResponseEntity.notFound().build();
+    return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("/userId")
+    public ResponseEntity<Student> deleteStudent(@PathVariable Long studentId){
+        Student deletedStudent = studentService.deleteStudent(studentId);
+        if (deletedStudent == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(deletedStudent);
+    }
+
+}
